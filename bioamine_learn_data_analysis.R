0dfc31ae3ffe1850187b18c7df2c93c01549ff4c
Haşim Hakanoğlu <hasimhakanoglu@Hasims-MacBook-Pro.local>
2024-11-26 13:49:32 -0600

    adust plot axis and remove model optimizers options

--- bioamine_learn_data_analysis.R
+++ bioamine_learn_data_analysis.R
@@ -45,29 +45,30 @@ acc_dist <- als_speedAccuracy %>%
   filter(str_detect(var, "M")) %>%
   ggplot(aes(x = score, fill = var)) +
   geom_histogram(position = "dodge", bins = 15, color = "black") +
-  labs(x = "Accuracy", y = "Number of bees") +
+  labs(x = "Accuracy", y = "Number of Bees") +
   scale_fill_manual(limits = c("indM", "socM"), 
                     labels = c("Individual", "Social"), 
                     values = c("white", "black")) +
   scale_y_continuous(expand = c(0,0), 
-                     limits = c(0, 14),
+                     # limits = c(0, 14),
                      breaks = seq(0, 14, 2)) +
-  scale_x_continuous(breaks = seq(0, 1, 0.1)) +
+  scale_x_continuous(breaks = seq(0, 1, 0.1),
+                     labels = as.character(seq(0, 1, 0.1))) +
   geom_vline(aes(xintercept = score_mean, linetype=var)) + 
-  geom_vline(aes(xintercept = 0.5), linetype = "solid") + 
+  geom_vline(aes(xintercept = 0.5), linetype = "dotted") + 
   scale_linetype_manual(limits=c("indM", "socM"),
                         labels=c("Individual", "Social"),
-                        values=c("dashed", "dotted")) +
+                        values=c("dashed", "solid")) +
   theme_classic() +
   theme(legend.position = "none",
         axis.title = element_text(size=12),
-        axis.text = element_text(size=12, color = "black"))
+        axis.text = element_text(size=10, color = "black"))
 
 speed_dist <- als_speedAccuracy %>%
   filter(str_detect(var, "S")) %>%
   ggplot( aes(x = score, fill = var)) +
-  geom_histogram(position = "dodge", bins = 25, color = "black") +
-  labs(x = "Learning speed", y = "Number of bees") +
+  geom_histogram(position = "dodge", bins = 30, color = "black") +
+  labs(x = "Speed", y = "Number of Bees") +
   scale_fill_manual(limits = c("indS", "socS"), 
                     labels = c("Individual", "Social"), 
                     values = c("white", "black")) +
@@ -78,16 +79,14 @@ speed_dist <- als_speedAccuracy %>%
   geom_vline(aes(xintercept = score_mean, linetype = var)) + 
   scale_linetype_manual(limits=c("indS", "socS"),
                         labels=c("Individual", "Social"),
-                        values=c("dashed", "dotted")) +
+                        values=c("dashed", "solid")) +
   theme_classic() +
   theme(legend.position = "none",
         axis.title = element_text(size=12),
-        axis.text = element_text(size=12, color = "black"))
+        axis.text = element_text(size=10, color = "black"))
 
-png("fig2_accuracy_speed_dist.png", width=2000, height=2000, res=300)
-(acc_dist / speed_dist) + 
-  plot_layout(guides = "collect") + 
-  plot_annotation(tag_levels = "A")
+png("fig2_accuracy_speed_dist.png", width=1500, height=2000, res=300)
+speed_dist/ acc_dist
 dev.off()
 
 # statistical tests
@@ -143,14 +142,12 @@ scaled_als <- data.frame("DA" = scale(als$DA, TRUE, TRUE),
                          "hive" = als$hive,
                          "trial" = als$trial)
 
-set.seed(1) # for reproducibility
+set.seed(2024) # for reproducibility
 
 socS_mod <- lmer(socS ~ (DA + Ser + OA + TA + Glu + age)^2
                  + (DA + Ser + OA + TA + Glu |hive)
                  + (DA + Ser + OA + TA + Glu |trial),
-                 data = scaled_als, 
-                 control = lmerControl(optimizer = "bobyqa",
-                             optCtrl = list(maxfun = 1e6)))
+                 data = scaled_als)
 
 socS_mod@optinfo$conv # check for optimizer convergence
 
@@ -162,9 +159,7 @@ plot(socS_resid) # plot diagnostics
 indS_mod <- lmer(indS ~ (DA + Ser + OA + TA + Glu + age)^2
                  + (DA + Ser + OA + TA + Glu|hive)
                  + (DA + Ser + OA + TA + Glu|trial),
-                 data = scaled_als,
-                 control = lmerControl(optimizer = "bobyqa",
-                                       optCtrl = list(maxfun = 1e6)))
+                 data = scaled_als)
 
 indS_mod@optinfo$conv # check for optimizer convergence
 
@@ -176,9 +171,7 @@ plot(indS_resid)
 socM_mod <- lmer(socM ~ (DA + Ser + OA + TA + Glu + age)^2
                  + (DA + Ser + OA + TA + Glu |hive)
                  + (DA + Ser + OA + TA + Glu |trial),
-                 data = scaled_als,
-                 control = lmerControl(optimizer = "bobyqa",
-                                       optCtrl = list(maxfun = 1e6)))
+                 data = scaled_als)
 
 socM_mod@optinfo$conv # check for optimizer convergence
 
@@ -190,9 +183,7 @@ plot(socM_resid)
 indM_mod <- lmer(indM ~ (DA + Ser + OA + TA + Glu + age)^2
                  + (DA + Ser + OA + TA + Glu|hive)
                  + (DA + Ser + OA + TA + Glu|trial),
-                 data = scaled_als,
-                 control = lmerControl(optimizer = "bobyqa",
-                                       optCtrl = list(maxfun = 1e6)))
+                 data = scaled_als)
 
 indM_mod@optinfo$conv # check for optimizer convergence
 
@@ -208,36 +199,28 @@ plot(indM_resid)
 socS_mod2 <- lmer(socS ~ DA + Ser + OA + TA + Glu + age
                   + (DA + Ser + OA + TA + Glu|hive)
                   + (DA + Ser + OA + TA + Glu|trial),
-                  data = scaled_als,
-                  control = lmerControl(optimizer = "bobyqa",
-                                        optCtrl = list(maxfun = 1e6)))
+                  data = scaled_als)
 
 socS_mod2@optinfo$conv # check for optimizer convergence
 
 indS_mod2 <- lmer(indS ~ DA + Ser + OA + TA + Glu + age
                   + (DA + Ser + OA + TA + Glu|hive)
                   + (DA + Ser + OA + TA + Glu|trial),
-                  data = scaled_als,
-                  control = lmerControl(optimizer = "bobyqa",
-                                        optCtrl = list(maxfun = 1e6)))
+                  data = scaled_als)
 
 indS_mod2@optinfo$conv # check for optimizer convergence
 
 socM_mod2 <- lmer(socM ~ DA + Ser + OA + TA + Glu + age
                   + (DA + Ser + OA + TA + Glu|hive)
                   + (DA + Ser + OA + TA + Glu|trial),
-                  data = scaled_als,
-                  control = lmerControl(optimizer = "bobyqa",
-                                       optCtrl = list(maxfun = 1e6)))
+                  data = scaled_als)
 
 socM_mod2@optinfo$conv # check for optimizer convergence
 
 indM_mod2 <- lmer(indM ~ DA + Ser + OA + TA + Glu + age
                   + (DA + Ser + OA + TA + Glu|hive)
                   + (DA + Ser + OA + TA + Glu|trial),
-                  data = scaled_als,
-                  control = lmerControl(optimizer = "bobyqa",
-                                        optCtrl = list(maxfun = 1e6)))
+                  data = scaled_als)
 
 indM_mod2@optinfo$conv # check for optimizer convergence
 
@@ -307,14 +290,14 @@ lmm_plot_theme <- theme(panel.background=element_blank(),
                         axis.title.x=element_text(size=8),
                         axis.title.y=element_text(size=8),
                         axis.ticks=element_blank(),
-                        plot.title=element_text(hjust=0.5, size=8),
+                        plot.title=element_text(hjust=0.5, size=10),
                         legend.position="top",
                         legend.text=element_text(size=8))
 
 # socS LMM term estimates and significance
 socS_plot <- ggplot(socS_est, aes(x=estimate, y=term, color=group, label=p.stars)) +
-  geom_point(size=2) + ylab("") + xlab("Estimate") + 
-  geom_text(vjust=0, nudge_y=-0.05, size=4, color=1) +
+  geom_point(size=3) + ylab("") + xlab("Estimate") + 
+  geom_text(vjust=0, nudge_y=-0.05, size=6, color=1) +
   geom_errorbar(aes(xmin=conf.low, xmax=conf.high, width=0.3)) +
   geom_vline(xintercept=0, linetype="dashed", color = "black", size=0.3) + 
   scale_color_manual(values=c("neg"="black", pos="grey"), guide="none") +
@@ -325,8 +308,8 @@ socS_plot <- ggplot(socS_est, aes(x=estimate, y=term, color=group, label=p.stars
 
 # indS LMM term estimates and significance
 indS_plot <- ggplot(indS_est, aes(x=estimate, y=term, color=group, label=p.stars)) +
-  geom_point(size=2) + ylab("") + xlab("Estimate") + 
-  geom_text(vjust=0, nudge_y=-0.05, size=4, color=1) +
+  geom_point(size=3) + ylab("") + xlab("Estimate") + 
+  geom_text(vjust=0, nudge_y=-0.05, size=6, color=1) +
   geom_errorbar(aes(xmin=conf.low, xmax=conf.high, width=0.3)) +
   geom_vline(xintercept=0, linetype="dashed", color = "black", size=0.3) + 
   scale_color_manual(values=c("neg"="black", pos="grey"), guide="none") +
@@ -337,8 +320,8 @@ indS_plot <- ggplot(indS_est, aes(x=estimate, y=term, color=group, label=p.stars
 
 # socM LMM term estimates and significance
 socM_plot <- ggplot(socM_est, aes(x=estimate, y=term, color=group, label=p.stars)) +
-  geom_point(size=2) + ylab("") + xlab("Estimate") + 
-  geom_text(vjust=0, nudge_y=-0.05, size=4, color=1) +
+  geom_point(size=3) + ylab("") + xlab("Estimate") + 
+  geom_text(vjust=0, nudge_y=-0.05, size=6, color=1) +
   geom_errorbar(aes(xmin=conf.low, xmax=conf.high, width=0.3)) +
   geom_vline(xintercept=0, linetype="dashed", color = "black", size=0.3) + 
   scale_color_manual(values=c("neg"="black", pos="grey"), guide="none") +
@@ -349,8 +332,8 @@ socM_plot <- ggplot(socM_est, aes(x=estimate, y=term, color=group, label=p.stars
 
 # indM LMM term estimates and significance
 indM_plot <- ggplot(indM_est, aes(x=estimate, y=term, color=group, label=p.stars)) +
-  geom_point(size=2) + ylab("") + xlab("Estimate") + 
-  geom_text(vjust=0, nudge_y=-0.05, size=4, color=1) +
+  geom_point(size=3) + ylab("") + xlab("Estimate") + 
+  geom_text(vjust=0, nudge_y=-0.05, size=6, color=1) +
   geom_errorbar(aes(xmin=conf.low, xmax=conf.high, width=0.3)) +
   geom_vline(xintercept=0, linetype="dashed", color = "black", size=0.3) + 
   scale_color_manual(values=c("neg"="black", pos="grey"), guide="none") +
@@ -359,7 +342,7 @@ indM_plot <- ggplot(indM_est, aes(x=estimate, y=term, color=group, label=p.stars
   lmm_plot_theme +
   theme(axis.text.y = element_text(face = c("plain", "bold", rep("plain", 3), "bold", rep('plain', 9))))
 
-png("lmm_estimates.png", width=3000, height=2500, res=400)
+png("lmm_estimates.png", width=3000, height=3500, res=400)
 # arranging aa plots in one figure and removing x-axis title
 comb_plots <- ggarrange(indS_plot + rremove("ylab") + rremove("xlab"), 
                         indM_plot + rremove("y.text") + rremove("xlab"), 
